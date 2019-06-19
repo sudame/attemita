bfd754127aec4d3b3fe1aa3c3cdedc82efcf3989
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-06-19 16:12:36 +0900

    add: pages

---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>login page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class LoginPage extends Vue {}
+</script>
---
+++ pages/person/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>person page {{ id }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { isNullOrUndefined } from 'util';
+
+@Component({
+    validate: function({ params }): boolean {
+        const id = params['id'];
+        return !isNullOrUndefined(id) && id !== '';
+    }
+})
+export default class TeamPage extends Vue {
+    get id(): string {
+        return this.$route.params['id'];
+    }
+}
+
+</script>
