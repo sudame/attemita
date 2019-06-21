aa1eee7cd7c6252ce2a1bbf36b025306c180bbab
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-06-21 16:14:46 +0900

    add: can get team uid

--- pages/index.vue
+++ pages/index.vue
@@ -13,28 +13,59 @@
       @click="submit()">
       送信
     </button>
-    <div
-      v-for="person in people"
-      :key="person.name">
-      <table>
-        <tr
-          v-for="(val, key) in person"
-          :key="key">
-          <td>{{ key }}</td>
-          <td>{{ val }}</td>
-        </tr>
-      </table>
+    <div>
+      <input
+        v-model="teamname"
+        type="text" />
+      <button @click="createTeam()">
+        チームを作る
+      </button>
+    </div>
+    <div>
+      <h3>teams</h3>
+      <div
+        v-for="team in teams"
+        :key="team.id">
+        <nuxt-link :to="`/team/${team.id}`">
+          {{ team.name }} | {{ team.id }}
+        </nuxt-link>
+      </div>
+    </div>
+    <div>
+      <h3>people</h3>
+      <div
+        v-for="person in people"
+        :key="person.name">
+        <table>
+          <tr
+            v-for="(val, key) in person"
+            :key="key">
+            <td>{{ key }}</td>
+            <td>{{ val }}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import firebase from '@/plugins/firebase';
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 import { getModule } from 'vuex-module-decorators';
 import  attemitaModule, { User } from '@/store/attemita';
 
 import { firestore } from 'firebase';
+
+class Team {
+    id: string;
+    name: string;
+
+    constructor(id: string, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+}
 
 class Person {
     name: string;
@@ -62,7 +93,10 @@ export default class Index extends Vue {
 
     private name: string = '';
     private people: Person[] = [];
+    private teams: Team[] = [];
     private unsubscribe: firebase.Unsubscribe | null = null;
+    private teamname: string = '';
+
 
     get user(): User | null{
         return this.attemitaStore.user;
@@ -81,11 +115,39 @@ export default class Index extends Vue {
                     });
                     this.people = updated;
                 });
+
+                firebase.firestore().collection('belongs').where('user', '==', user.uid).onSnapshot(qSnap => {
+                    const updated: Team[] = [];
+                    qSnap.forEach(dSnap => {
+                        const teamRef: firestore.DocumentReference = dSnap.data()['team'];
+                        teamRef.get().then(teamSnap => {
+                            const data = teamSnap.data();
+                            if(data !== undefined){
+                                updated.push(new Team(teamSnap.id, data['name']));
+                            }
+                        });
+                    });
+                    this.teams = updated;
+                });
             } else {
                 this.people = [];
+                this.teams = [];
                 if(this.unsubscribe !== null) this.unsubscribe();
             }
         });
+    }
+
+    async createTeam() {
+        if(this.user !== null){
+            const ref = await firebase.firestore().collection('teams').add({
+                name: this.teamname
+            });
+
+            firebase.firestore().collection('belongs').add({
+                team: ref,
+                user: this.user.uid,
+            });
+        }
     }
 
     logout() {
--- pages/team/_id.vue
+++ pages/team/_id.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>team page {{ id }}</h1>
+    <div
+      v-for="user in users"
+      :key="user">
+      {{ user }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import { isNullOrUndefined } from 'util';
+import firebase from '@/plugins/firebase';
 
 @Component({
     validate: function({ params }): boolean {
@@ -15,6 +21,20 @@ import { isNullOrUndefined } from 'util';
     }
 })
 export default class TeamPage extends Vue {
+
+    mounted() {
+        const teamRef: firebase.firestore.DocumentReference = firebase.firestore().collection('teams').doc(this.id);
+        firebase.firestore().collection('belongs').where('team', '==', teamRef).get().then(qSnap => {
+            const updated: string[] = [];
+            qSnap.forEach(dSnap => {
+                updated.push(dSnap.data()['user']);
+            });
+            this.users = updated;
+        });
+    }
+
+    private users: string[] = [];
+
     get id(): string {
         return this.$route.params['id'];
     }
