4de3a5ac1111e366185219472e5a30955a9d39f6
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-06-23 21:40:49 +0900

    add: seamless typing

--- components/resizableTextArea.vue
+++ components/resizableTextArea.vue
@@ -2,8 +2,10 @@
   <textarea
     id="textarea"
     :value="value"
+    :placeholder="placeholder"
     name=""
-    @input="onInput"></textarea>
+    @input="onInput"
+    @keydown.meta.enter="metaEnter"></textarea>
 </template>
 
 <script lang="ts">
@@ -11,20 +13,45 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 
 @Component
 export default class ResizableTextArea extends Vue {
-    @Prop(String) readonly value: string | undefined;
+    @Prop(String) readonly placeholder: string | undefined;
+
+    value: string = '';
 
     mounted () {
       const t: HTMLTextAreaElement = this.$el as HTMLTextAreaElement;
-      t.style.height = '';
-      t.style.height = t.scrollHeight + 5 + 'px';
+      t.style.minHeight = '';
+      t.style.minHeight = t.scrollHeight + 5 + 'px';
       this.$emit('input', t.value);
     }
 
     onInput(e: UIEvent) {
       const t = e.target as HTMLTextAreaElement;
-      t.style.height = '';
-      t.style.height = t.scrollHeight + 5 + 'px';
+      t.style.minHeight = '';
+      t.style.minHeight = t.scrollHeight + 5 + 'px';
+      this.value = t.value;
       this.$emit('input', t.value);
+    }
+
+    metaEnter(e: UIEvent) {
+      if(this.value  === undefined) return;
+      const textarea: HTMLTextAreaElement = e.target as HTMLTextAreaElement;
+      const sentence: string = this.value;
+      const len: number = sentence.length;
+      const pos: number = textarea.selectionStart;
+      const endPos: number = textarea.selectionEnd;
+
+      const before: string = sentence.substr(0, pos);
+      const word = '\n## ';
+      const after: string = sentence.substr(pos, len);
+      this.value = before + word + after;
+      this.$emit('input', this.value);
+      this.$nextTick(() => {
+        textarea.focus();
+        textarea.selectionEnd = endPos + word.length;
+        const t = e.target as HTMLTextAreaElement;
+        t.style.minHeight = '';
+        t.style.minHeight = t.scrollHeight + 5 + 'px';
+      });
     }
 }
 </script>
